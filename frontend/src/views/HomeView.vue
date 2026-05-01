6f5f4972fc9f5f87a145d29b9ec53497a1fb4a33
tobihã169 <otachienti169@gmail.com>
2026-05-01 19:49:13 +0700

    ok

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -90,7 +90,7 @@
 <script setup>
 import { useI18n } from 'vue-i18n';
 import {
-  HeadphonesIcon,
+  SpeakerWaveIcon,
   PencilSquareIcon,
   ShieldCheckIcon,
   BoltIcon,
@@ -101,7 +101,7 @@ const { t } = useI18n();
 
 const features = [
   {
-    icon: HeadphonesIcon,
+    icon: SpeakerWaveIcon,
     title: t('home.features.support'),
     description: 'Hỗ trợ 24/7 qua Discord và Email'
   },
